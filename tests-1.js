36c6c6043ea447d7f34f14c678091030df6161c1
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-09-24 16:00:39 -0400

    js Updates

---
+++ tests-1.js
@@ -0,0 +1,11 @@
+// -------Basic consol-log-------
+// console.log('Hello World');
+
+//------- Styled console log-------
+//console.log("%cHello World", "color: blue; font-size: 40px");
+
+//------ Using +
+//console.log("Hello " + "there, " + "World");
+
+//------ Using , inplace of +
+console.log("Hello ", "there, ", "World")
