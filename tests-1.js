390b66b62adc9183267e8a63e2ec97bbef0ba9a4
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-09-24 16:54:13 -0400

    js Update 2

--- tests-1.js
+++ tests-1.js
@@ -8,4 +8,28 @@
 //console.log("Hello " + "there, " + "World");
 
 //------ Using , inplace of +
-console.log("Hello ", "there, ", "World")
+//console.log("Hello ", "there, ", "World")
+
+//-------Using Variables
+/* var person = 'John';
+var greeting = 'Hello';
+
+console.log(greeting, person); */
+
+//----- Declaring Variables Exercise
+var petDog = 'Rex'; // variable created to store dogs name
+var petCat = 'Pepper'; // variable created to store dogs name
+var catSound = 'purr'; // variable created to store cat sound
+var dogSound = 'Woof'; // variable created to store dog sound
+
+console.log(petDog); // display dogs name in the console
+console.log(petCat); // display cats name in the console
+
+console.log("My pet dog's name is: ", petDog); //displays message + dogs name in console
+console.log("My pet cat's name is: ", petCat); //displays message + dogs name in console
+
+console.log(petDog, 'says', dogSound); //displays message + dogs sound in console
+console.log(petCat, 'says', catSound); //displays message + cats sound in console
+
+catSound = 'meow'; // variable created to change stored cats sound
+console.log(petCat, 'now says', catSound); //displays message + updated cat sound in console
