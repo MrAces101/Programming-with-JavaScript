882a48caf3278aca3455c174d94a6f22df19ddf1
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-09-24 18:30:51 -0400

    Operator Exercises

--- tests-1.js
+++ tests-1.js
@@ -66,3 +66,33 @@ console.log(petCat, 'now says', catSound); //displays message + updated cat soun
 // console.log(3 < 2);
 // console.log( 2 > 3);
 // console.log(10 == 10);
+
+//----------Exercises----------
+
+// You are coding an RPG game, where each character has certain skill levels based on the value saved in their score.
+
+/* var score = 8;
+console.log("Mid-level skills:", score > 0 && 10 > score); */
+
+// Imagine you are coding a video game. Currently, you’re about to code some snippets related to the game over condition.
+
+
+/* var timeRemaining = 0;
+var energy = 10;
+
+console.log("Game Over", timeRemaining  == 0 || energy == 0) */
+
+// You need to code a small program that takes a number and determines if it's an even number (like 2, 4, 6, 8, 10).
+
+/* var num1 = 2;
+var num2 = 5;
+
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+*/
