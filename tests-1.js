ccb1b2b3ad0a0b2f6c407232c701e52bf0ae3f59
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-09-26 21:28:28 -0400

    Using Classes

--- tests-1.js
+++ tests-1.js
@@ -736,3 +736,23 @@ console.log('This line never runs'); // wont run */
 
 
 //--------------------CLASSES-----------
+
+// class Car {
+//     constructor(color, speed) {
+//         this.color = color;
+//         this.speed = speed;
+//     }
+
+//     turboOn() {
+//         console.log('Turbo is on!')
+//     }
+// }
+
+// const car1 = new Car('red', 120)
+
+// //car1.turboOn();
+
+// console.log(car1);
+
+//-------------------Inheritance
+
